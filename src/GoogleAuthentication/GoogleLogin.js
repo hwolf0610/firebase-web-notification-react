60d802a99300585d7dc1676269dc76fa6fe5fb7d
hwolf0610 <hwolf0610@hotmail.com>
2020-12-30 01:00:45 -0600

    google Authentication made

--- src/GoogleAuthentication/GoogleLogin.js
+++ src/GoogleAuthentication/GoogleLogin.js
@@ -4,8 +4,7 @@ import { GoogleLogin } from 'react-google-login';
 // refresh token
 import { refreshTokenSetup } from '../utils/refreshToken';
 
-const clientId =
-  '707788443358-u05p46nssla3l8tmn58tpo9r5sommgks.apps.googleusercontent.com';
+const clientId =process.env.REACT_APP_Firebase_ClientId;
 
 function Login() {
   const onSuccess = (res) => {
